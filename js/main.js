b05332c32a8c5f24626d6dcb92db5cb3ac7e6fda
David de Oliviera <david@hotmail.com>
2020-12-18 19:13:26 -0300

    commit on master

--- js/main.js
+++ js/main.js
@@ -36,6 +36,18 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+WebFontConfig = {
+    google: { families: [ 'Open+Sans::latin' ] }
+    };
+    (function() {
+    var wf = document.createElement('script');
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})();
+
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
