0059fca3dbc20ef756febb1ce902c3395236a01a
David de Oliviera <david@hotmail.com>
2020-12-17 22:47:19 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+document.querySelectorAll('section').forEach(section => {
+    section.addEventListener('mouseenter', function() {
+        this.firstElementChild.classList.add('section-hover')
+    }, false)
+    section.addEventListener('mouseleave', function() {
+        this.firstElementChild.classList.remove('section-hover')
+    }, false)
+})
+
+AOS.init()
+
+function myFunction() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Click to copy my email";
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
